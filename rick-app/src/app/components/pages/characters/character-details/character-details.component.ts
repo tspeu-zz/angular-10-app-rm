e670d03d81cd00b2e11fd6a55ee3de47ed4071f1
jm_b <jmbanchero@gmail.coom>
2021-01-12 19:58:05 +0000

    details

--- rick-app/src/app/components/pages/characters/character-details/character-details.component.ts
+++ rick-app/src/app/components/pages/characters/character-details/character-details.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+//
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+//
+import { Character } from '@shared/componets/interface/character.interface';
+import { CharacterService } from '@app/shared/services/character.service';
 
 @Component({
   selector: 'app-character-details',
@@ -7,9 +15,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterDetailsComponent implements OnInit {
 
-  constructor() { }
+  // character: Character;
+  character$: Observable<Character>;
+
+  constructor(private route: ActivatedRoute,
+              private charService: CharacterService,
+              private location: Location) { }
 
   ngOnInit(): void {
+    //buscar el personaje que vien por la url
+    this.route.params.pipe( take(1) )
+    .subscribe( (params) =>{
+      const id = params['id'];
+      this.character$ = this.charService.getDetails(id);
+    });
+  }
+
+  onGoBack() {
+    this.location.back();
+    // window-history.back()
   }
 
 }
